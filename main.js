970130d2d16ac69adbbf4210511ec2bb60f22b47
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-24 15:42:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// // // MILESTONE 1 
+const list=[
+    'Ryzen 5800',
+    'RTX 4090',
+    '32gb Trident Z',
+    'MSI z390',
+    'Cooler Master Liquid 360',
+]
+
+console.log(list)
+for (let x = 0; x < list.length; x++) {
+    console.log(x , "ciclo for")
+}
+
+// // // MILESTONE 2
+
+x = 0
+while(x < list.length){
+    x++;
+    console.log(x , "ciclo while");
+    
+}
